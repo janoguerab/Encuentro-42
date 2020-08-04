5b73271639cc391369fda980b54d5ce09be35f48
Alejandro Noguera <janoguerab@unal.edu.co>
2020-08-03 21:19:22 -0500

    Edited index to require moment and cool-images

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const moment= require("moment");
-
-
-var fecha = new moment("31/12/2019","DD/MM/YYYY");
-var formateada=fecha.format("MM/DD/YY");
-console.log(formateada);
+let moment = require('moment')
+let coolImages = require('cool-images')
+let fs = require('fs')
+let cool = coolImages.many(1920, 1080, 10);
+let fecha = new moment().local().format("YYYY/MM/DD hh:mm:ss a");
+setTimeout(function(){
+    insertText(fecha + '\n' + coolImages.one() + '\n')
+}, 1000)
+setTimeout(function(){
+    insertText(fecha + '\n')
+    cool.forEach(element => {
+        insertText(element + '\n') ;
+    });
+}, 3000)
+function insertText(element){
+    fs.appendFile('./log.txt', element, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
